2f431d4085bd4a09996e888c75972612efcb9b73
Kevin Eddye <kevneddy@gmail.com>
2015-11-17 17:11:07 -0800

    Add click action to grid

--- battleShip2.js
+++ battleShip2.js
@@ -1,7 +1,8 @@
 function makeGrid(color) {
     var grid = document.createElement("div");
-    grid.style.width = "12.%";
-    grid.style.paddingBottom = "12.%";
+    grid.style.width = "13.26%";
+    grid.style.paddingBottom = "13.26%";
+    // grid.style.paddingBottom = "14.28%";
     grid.style.float = "left";
     grid.style.background = color;
     grid.style.borderStyle = 'solid';
@@ -18,6 +19,9 @@ function createGrid(n) {
   var grid = null;
   var container = document.createElement("div");
   container.id = "container";
+  container.style.padding = "0";
+
+  container.setAttribute('class', 'group');
 
   for (var i = 0; i < n; i++) {
      color = "white";
@@ -26,23 +30,40 @@ function createGrid(n) {
      grid.setAttribute('class', 'tile')
 
     container.appendChild(grid);
-   
   }
  document.body.appendChild(container);
 }
 
 
-createGrid(42);
+createGrid(49);
 
 // 
 
 var tiles = document.getElementsByClassName('tile');
 
-for(var i=0; i<tiles.length; i++){
+for(var i=0; i < tiles.length; i++){
   tiles[i].addEventListener('click',changeColor);
 }
-// function changeColor(event){
-//   event.target.style.backgroundColor: red;
+function changeColor(event){
+  this.style.backgroundColor = "red";
+}
+
+
+// function createScoreBox(n){
+//   var box = null;
+//   var container = document.createElement('div');
+//   container.id = 'scorebox'
+//   container.setAttribute('class', 'score');
+
+//   var score =o;
+
+//   function drawScore (n) {
+//          ctx.font = "16px Arial";
+//     ctx.fillStyle = "#0095DD";
+//     ctx.fillText("Score: "+score);
+//    } 
+
 // }
+
 
 
