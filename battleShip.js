e5df365516ed5ebc08752ebe288a04d613162fb1
Kevin Eddye <kevneddy@gmail.com>
2015-11-18 15:34:15 -0800

    Add eventListener to to submit btn, saves username to localStorage

---
+++ battleShip.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(e) { 
+  console.log("btn next")
+  var val = document.getElementsByTagName('input')[name="username"].value
+"cho"
+  localStorage.setItem('username', val);
+});
