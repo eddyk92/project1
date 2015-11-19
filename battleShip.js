98b0c38cbf8d20d6bcc4768ef700f73fae0e5328
Kevin Eddye <kevneddy@gmail.com>
2015-11-18 16:54:08 -0800

    Add localstorage to usernam, email, password

--- battleShip.js
+++ battleShip.js
@@ -1,8 +1,25 @@
 var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function(e) { 
-  console.log("btn next")
+btn.addEventListener("click", function() { 
   var val = document.getElementsByTagName('input')[name="username"].value
-"cho"
+
   localStorage.setItem('username', val);
+});
+
+// accessing
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() { 
+  var val = document.getElementsByTagName('input')[name="email"].value
+
+  localStorage.setItem('email', val);
+});
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() { 
+  var val = document.getElementsByTagName('input')[name="password"].value
+
+  localStorage.setItem('password', val);
 });
