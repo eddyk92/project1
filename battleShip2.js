7b392adcce58dcab49b8db29da58e84b9ce4cdaf
Kevin Eddye <kevneddy@gmail.com>
2015-11-20 14:48:12 -0800

    Add randomize div

--- battleShip2.js
+++ battleShip2.js
@@ -1,5 +1,8 @@
+// make game board
 function makeGrid(color) {
     var grid = document.createElement("div");
+    // grid.style.width = "200px";
+    // grid.style.paddingBottom = "200px";
     grid.style.width = "40px";
     grid.style.paddingBottom = "40px";
     // grid.style.paddingBottom = "14.28%";
@@ -14,7 +17,7 @@ function makeGrid(color) {
     return grid;
 }
 
-
+// make 
 function createGrid(n) {
   var grid = null;
   var container = document.createElement("div");
@@ -22,20 +25,49 @@ function createGrid(n) {
   container.style.padding = "0";
 
   container.setAttribute('class', 'group');
+  
+  n = Math.sqrt(n);
 
-  for (var i = 0; i < n; i++) {
+  for (var i = 1; i < n; i++) {
+    for (var j = 1; j < n; j++) {
+      var coords = (i + "") + (j + "");
+     
       color = "#99d6ff";
 
       grid = makeGrid(color);
-      grid.setAttribute('class', 'tile')
+      grid.setAttribute('id', coords);
 
       container.appendChild(grid);
+    }
   }
+
+
   document.body.appendChild(container);
 }
 
+function randomShips() {
+  var xCoord = Math.floor(Math.random() * 9) + 1;
+  var yCoord = Math.floor(Math.random() * 9) + 1;
 
-createGrid(72);
+  return xCoord.toString() + yCoord.toString();
+}
+
+// var ship = {
+  document.getElementById(randomShips())
+  // document.randomShips.style.background = 'green';
+// }
+// document.randomShips(data-ship) = true;
+// how would i change the background color of this div
+
+
+// use randomShps return--e.g., "88"--to find the div with an id of "88"
+// give that div an attribute with a key "data-ship" and value true
+
+
+
+
+// createGrid(14);
+ createGrid(89);
 
 
 var tiles = document.getElementsByClassName('tile');
@@ -57,46 +89,6 @@ console.log(name);
 
 
 document.userName
-
-
-
-
-// game.prototype.shotListener = function(e) {
-//   var self = e.target.self;
-//   var x = parseInt(e.target.getAttribute('data-x'), 9)
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
